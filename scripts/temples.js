38c8aed9cb9a9906a3e397ee37b73d8d72e47818
Ticsilva85 <tiago.liverys@gmail.com>
2026-01-25 01:05:58 -0300

    week 2 templo album updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,15 @@
-console.log("JS carregado")
-
 const lastModified = document.lastModified;
 
 document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;console.log("JS carregado");
+
+const menuButton = document.getElementById("menu-toggle");
+const navList = document.querySelector("nav ul");
+
+menuButton.addEventListener("click", () => {
+    navList.classList.toggle("open");
+
+    const isOpen = navList.classList.contains("open");
+    menuButton.setAttribute("aria-expanded", isOpen);
+
+    menuButton.textContent = isOpen ? "✖" : "☰";
+});
