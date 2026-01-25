95df339353fd287f21a4e52075ac6b53bb150a7b
Ticsilva85 <tiago.liverys@gmail.com>
2026-01-25 00:21:16 -0300

    week 01 temple album activity updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,20 +3,3 @@ console.log("JS carregado")
 const lastModified = document.lastModified;
 
 document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;console.log("JS carregado");
-
-// Footer
-const yearSpan = document.getElementById("currentyear");
-yearSpan.textContent = new Date().getFullYear();
-
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent =
-  `Last Modification: ${lastModified}`;
-
-// Hamburger menu
-const menuButton = document.getElementById("menu");
-const nav = document.getElementById("navigation");
-
-menuButton.addEventListener("click", () => {
-  nav.classList.toggle("open");
-  menuButton.textContent = nav.classList.contains("open") ? "✖" : "☰";
-});
