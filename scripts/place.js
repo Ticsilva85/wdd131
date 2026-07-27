542e809e17c6ae5a1e00d1eef2204469fbd89926
Ticsilva85 <tiago.liverys@gmail.com>
2026-07-26 23:45:22 -0300

    week 03 assignment updated

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,18 @@ currentyear.innerHTML = `<span id="currentyear">${today.getFullYear()}</span>`;
 
 const lastModified = document.getElementById("lastModified");
 lastModified.innerHTML = document.lastModified;
+
+const temperature = 5;
+const windSpeed = 12;
+
+function calculateWindChill(temp, speed) {
+    return 13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16));
+}
+
+const windChill = document.querySelector("#windChill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent = `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
