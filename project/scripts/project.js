0c6cb4e9e27b8cae6285dd24def5125fe4a5cea4
Ticsilva85 <tiago.liverys@gmail.com>
2026-08-11 23:09:01 -0300

    header mobile updated with menu hamburger

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -7,3 +7,12 @@
 // lastModified.innerHTML = document.lastModified;
 
 document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+// === HAMBURGER MENU ===
+const mainnav = document.querySelector('.header-nav');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show'); 
+    hambutton.classList.toggle('show');
+});
