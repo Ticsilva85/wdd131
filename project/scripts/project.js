c9a83e41cbef6f1aba6db07dcc3108e32b7a0e71
Ticsilva85 <tiago.liverys@gmail.com>
2026-08-12 23:45:42 -0300

    project updated

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -29,3 +29,24 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 4000);
+
+// === TEXT CAROUSEL ===
+
+const textSlides = document.querySelectorAll(".text-slide");
+
+let currentTextSlide = 0;
+
+function nextTextSlide() {
+
+    textSlides[currentTextSlide].style.opacity = "0";
+
+    currentTextSlide++;
+
+    if (currentTextSlide >= textSlides.length) {
+        currentTextSlide = 0;
+    }
+
+    textSlides[currentTextSlide].style.opacity = "1";
+}
+
+setInterval(nextTextSlide, 6000);
